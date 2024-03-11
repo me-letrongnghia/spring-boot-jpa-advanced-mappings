0b0c01025c7e59727643be42ecb4c142544215f5
letrongnghia <nghialee2301@gmail.com>
2024-03-11 20:13:20 +0700

    bi-directional

---
+++ 02-jpa-one-to-one-bi/src/main/java/com/nghiale/cruddemo/dao/AppDAO.java
@@ -0,0 +1,11 @@
+package com.nghiale.cruddemo.dao;
+
+import com.nghiale.cruddemo.entity.Instructor;
+import com.nghiale.cruddemo.entity.InstructorDetail;
+
+public interface AppDAO {
+    void save(Instructor theInstructor);
+    Instructor findInstructorById(int theId);
+    InstructorDetail findInstructorDetailById(int theId);
+    void deleteInstructorById(int theId);
+}
---
+++ 02-jpa-one-to-one-bi/src/main/java/com/nghiale/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,45 @@
+package com.nghiale.cruddemo.dao;
+
+import com.nghiale.cruddemo.entity.Instructor;
+import com.nghiale.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+
+    // define field for entityManager
+    private EntityManager entityManager;
+
+    // inject entityManager using constructor injection
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+        return entityManager.find(Instructor.class, theId);
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int theId) {
+        return entityManager.find(InstructorDetail.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+
+        Instructor instructor = entityManager.find(Instructor.class, theId);
+        entityManager.remove(instructor);
+    }
+
+}
